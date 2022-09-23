fd67ce8e8955f4dfa035044a1c11dcd3e8c4c9df
shameedjob <shameed.job.32@gmail.com>
2022-09-22 22:49:12 -0400

    Updated constructor for staff with test data

--- src/main/java/com/mycompany/agency/Staff.java
+++ src/main/java/com/mycompany/agency/Staff.java
@@ -13,13 +13,14 @@ public class Staff
     public Staff()
     {
         staffList = new StaffMember[6];
-        staffList[0]  = new StaffEmployee("Moaath Alrajab", " 25 New Tree Line", " 516-101-3131", " 000-000-1234", 21900.5);
-        staffList[1]  = new StaffEmployee("Moaath Alrajab", " 25 New Tree Line", " 516-101-3131", " 000-000-1234", 21900.5);
-        staffList[2]  = new StaffEmployee("Moaath Alrajab", " 25 New Tree Line", " 516-101-3131", " 000-000-1234", 21900.5);
-        staffList[2]  = new StaffEmployee("Moaath Alrajab", " 25 New Tree Line", " 516-101-3131", " 000-000-1234", 21900.5);
-        staffList[2]  = new StaffEmployee("Moaath Alrajab", " 25 New Tree Line", " 516-101-3131", " 000-000-1234", 21900.5);
+        staffList[0]  = new Executive("Moaath Alrajab", "25 New Tree Line", "516-101-3131", "000-000-1234", 21900.5);
+        staffList[1]  = new StaffEmployee("John", "456 Off Line", "516-555-0101", "516-65-4321",  1546.15);
+        staffList[2]  = new StaffEmployee("William", "789 Off Rocker", "516-555-0000", "631-20-30404",  1669.23);
+        staffList[3]  = new TempEmploee("Sam", "678 Fifth Ave.", "516-555-0690", "516-47-3625",  622.0);
+        staffList[4]  = new StaffMember("Alex", "987 Wood Blvd.", "516-555-83741");
+        staffList[5]  = new StaffMember("Maya", "1 Wood Blvd.", "516-555-4312");
         
-        ((Executive)staffList[0]).awardBonus(500.00);
+       ((Executive)staffList[0]).awardBonus(500.00);
 
         ((TempEmploee)staffList[3]).addHours(40);
     }
