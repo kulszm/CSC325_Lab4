afd4df0b7ebfdd60673fffab9e464422e4b109a9
shameedjob <shameed.job.32@gmail.com>
2022-09-26 22:07:49 -0400

    Fixed issues in Intern, TempEmploee, and StaffMember classes

--- src/main/java/com/mycompany/agency/StaffEmployee.java
+++ src/main/java/com/mycompany/agency/StaffEmployee.java
@@ -3,7 +3,7 @@ package com.mycompany.agency;
 
     // TODO 1: Make StaffEmployee a child of StaffEmploee
 
-public class StaffEmployee 
+public class StaffEmployee extends StaffMember
 {
     protected String socialSecurityNumber;
     protected double payRate;
@@ -27,14 +27,18 @@ public class StaffEmployee
     //-----------------------------------------------------------------
     public String toString()
     {
-    
+        String sOutput = super.toString();
+        sOutput += String.format("Social Security Number: %s", socialSecurityNumber) + "/n";
+        sOutput += String.format("Pay Rate: %s", socialSecurityNumber) + "/n";
+        return sOutput;
     }
 
     //-----------------------------------------------------------------
     // TODO 4:  Returns the pay rate for this staff employee.
     //-----------------------------------------------------------------
+    @Override
     public double pay()
     {
-       
+       return payRate;
     }
 }
